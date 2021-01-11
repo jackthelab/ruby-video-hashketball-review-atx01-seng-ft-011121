88b3a147b14a7fbe08ca3909c7b41a4972389480
Jacob Hampton <jhampton792@gmail.com>
2021-01-11 02:53:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,14 +128,3 @@ def game_hash
 end
 
 # Write code here
-
-def num_points_scored(player)
-  
-  game_hash.each do | i |
-    i[:players].each | x |
-      if x[:player_name] == player
-        return x[:points]
-      end
-  end
-  
-end
